e78aad9ccecf41966a5efe9d87e9aa66cc0090fb
Fay-chebby <chebetf034@gmail.com>
2025-03-05 14:50:05 +0300

    done

--- food/src/App.tsx
+++ food/src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import {BrowserRouter,Routes,Route}from 'react-router-dom'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 import Login from "./Pages/Login/Login";
 import Signup from "./Pages/Signup/Signup";
 import Resetpassword from "./Pages/Reset password/Resetpassword";
+import Home from "./Pages/Home/home";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/reset-password" element={<Resetpassword />} />
-        </Routes>
-      </BrowserRouter>
-
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/reset-password" element={<Resetpassword />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
